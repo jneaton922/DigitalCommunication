6ec332b9e85ed953097389977f84a81d251d7f13
Josh Eaton <jneaton922@gmail.com>
2019-03-25 13:36:49 -0400

    HW8 sims updated

--- HW8/bpsk_psd.m
+++ HW8/bpsk_psd.m
@@ -3,16 +3,16 @@ close all
 clc;
 
 % Simulation Parameters
-num_bits = 32;
+num_bits = 128;
 R = 1e3;
-spb = 32;
+spb = 256;
 fc = 1e5;
 Ac = 1;
 Ap = 1;
 values = Ap*[-1 1];
 num_signals = 1000;
-plot_delay=5e-3;
-skip_plots = 5;
+plot_delay=1e-3;
+skip_plots = 50;
 update_plot = true;
 
 Tb = 1/R;
@@ -22,11 +22,13 @@ r = 0.4;
 
 N =spb*num_bits;
 fs = spb/Tb;
+bin_res = fs/N;
 freq = -fs/2:fs/N:fs/2 - fs/N;
 
 A = values(1);
 Ff = fftshift(fft(pulse(2:end),N));
 theor_psd = Ac^2 * Tb/4 * (abs(Ff)/spb).^2;
+theor_psd = circshift(theor_psd,floor(fc/bin_res)) + circshift(theor_psd,ceil(-fc/bin_res));
 Psd=zeros(1,N);
 
 trunc_inds = (k)*spb;
@@ -37,14 +39,11 @@ for i=1:num_signals
     data = bits(num_bits,values);
     [t,bb] = baseband_signal(data,pulse,k,Tb,spb);
     bb = bb(1+trunc_inds:length(bb)-trunc_inds);
-    Sf = ((Tb)/spb)*fftshift(fft(bb));
-    Psd = Psd + (abs(Sf).^2)./(T);
-    if i == 1
-        plot(freq,abs(Sf));
-        title('|S(f)|');
-        figure();
-    end
+    t = t(1+trunc_inds:length(t)-trunc_inds);
     
+    mod_signal = bpsk(bb,t,fc,Ac);
+    Sf = ((Tb)/spb)*fftshift(fft(mod_signal));
+    Psd = Psd + (abs(Sf).^2)./(T);    
     if mod(i-1,skip_plots) == 0 && update_plot
         plot(freq,Psd/i,freq,theor_psd);
         legend({'Simulated','Theoretical'});
